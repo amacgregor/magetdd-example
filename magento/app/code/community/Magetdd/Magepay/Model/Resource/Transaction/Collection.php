3cde4045d9b14c13834fbd2cdd94322b09f9ac42
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-09-09 10:50:11 -0400

    Update the Transaction collection extend the right models

--- magento/app/code/community/Magetdd/Magepay/Model/Resource/Transaction/Collection.php
+++ magento/app/code/community/Magetdd/Magepay/Model/Resource/Transaction/Collection.php
@@ -1,6 +1,9 @@
 <?php
 
-class Magetdd_Magepay_Model_Resource_Transaction_Collection extends Mage_Core_Model_Abstract
+class Magetdd_Magepay_Model_Resource_Transaction_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
-
+  protected function _construct()
+  {
+    $this->_init('magetdd_magepay/transaction');
+  }
 }
