8762c73f6e6259c98efc2d7cb6bc5cc6b975c925
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-09-09 09:18:24 -0400

    Add MagePay resource model and matching spec

---
+++ magento/app/code/community/Magetdd/Magepay/Model/Resource/Transaction.php
@@ -0,0 +1,9 @@
+<?php
+
+class Magetdd_Magepay_Model_Resource_Transaction extends Mage_Core_Model_Resource_Db_Abstract
+{
+    protected function _construct()
+    {
+
+    }
+}
---
+++ spec/app/code/community/Magetdd/Magepay/Model/Resource/TransactionSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class Magetdd_Magepay_Model_Resource_TransactionSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Magetdd_Magepay_Model_Resource_Transaction');
+    }
+}
