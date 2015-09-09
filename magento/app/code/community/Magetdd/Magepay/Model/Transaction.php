a66f34540ad611a001e3c732cfe1a2af25e40ef0
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-09-09 15:24:52 -0400

    Transaction model update with getters

--- magento/app/code/community/Magetdd/Magepay/Model/Transaction.php
+++ magento/app/code/community/Magetdd/Magepay/Model/Transaction.php
@@ -2,5 +2,29 @@
 
 class Magetdd_Magepay_Model_Transaction extends Mage_Core_Model_Abstract
 {
+    public function __construct()
+    {
+        parent::_construct();
+        $this->_init('magetdd_magepay/transaction');
+    }
 
+    public function getTransactionId()
+    {
+        return $this->_getData('transaction_id');
+    }
+
+    public function getOrderId()
+    {
+        return $this->_getData('order_id');
+    }
+
+    public function getState()
+    {
+        return $this->_getData('state');
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->_getData('created_at');
+    }
 }
