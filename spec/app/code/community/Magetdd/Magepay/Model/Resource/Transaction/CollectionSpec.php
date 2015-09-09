c88c39e0305047e0ef3790beee0cae2a45c10168
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-09-09 10:54:13 -0400

    Override the Abstract collection class

--- spec/app/code/community/Magetdd/Magepay/Model/Resource/Transaction/CollectionSpec.php
+++ spec/app/code/community/Magetdd/Magepay/Model/Resource/Transaction/CollectionSpec.php
@@ -11,4 +11,19 @@ class Magetdd_Magepay_Model_Resource_Transaction_CollectionSpec extends ObjectBe
     {
         $this->shouldHaveType('Magetdd_Magepay_Model_Resource_Transaction_Collection');
     }
+
+    function it_extends_the_resource_db_collection()
+    {
+      $this->shouldBeAnInstanceOf('Mage_Core_Model_Resource_Db_Collection_Abstract');
+    }
+}
+
+abstract class Mage_Core_Model_Resource_Db_Collection_Abstract {
+  public function __construct($resource = null)
+  {
+    return true;
+  }
+  protected function _init($mode, $resource) {
+    return true;
+  }
 }
