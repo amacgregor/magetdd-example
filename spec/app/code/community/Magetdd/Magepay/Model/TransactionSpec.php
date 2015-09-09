04203cbd90cc72e4bbaf2167fd2706fda75f0764
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-09-09 14:08:46 -0400

    Update the Transaction Spec to check for Transaction properties

--- spec/app/code/community/Magetdd/Magepay/Model/TransactionSpec.php
+++ spec/app/code/community/Magetdd/Magepay/Model/TransactionSpec.php
@@ -11,4 +11,24 @@ class Magetdd_Magepay_Model_TransactionSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Magetdd_Magepay_Model_Transaction');
     }
+
+    function it_should_have_a_transaction_id()
+    {
+        $this->getTransactionId()->shouldReturn(99999999999);
+    }
+
+    function it_should_have_an_order_id()
+    {
+        $this->getOrderId()->shouldReturn(888888888888);
+    }
+
+    function it_should_have_a_state()
+    {
+        $this->getState()->shouldReturn('processing');
+    }
+
+    function it_should_have_a_created_at_date()
+    {
+        $this->getCreatedAt()->shouldReturn('2015-09-09');
+    }
 }
