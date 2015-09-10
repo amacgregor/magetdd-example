ef9d42319e9a6974b2e3abdccb50432af7786cda
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-09-10 11:59:43 -0400

    Update the IndexController

--- magento/app/code/community/Magetdd/Magepay/controllers/IndexController.php
+++ magento/app/code/community/Magetdd/Magepay/controllers/IndexController.php
@@ -3,6 +3,10 @@ class Magetdd_Magepay_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo 'test';
+      $this->loadLayout();
+      $this->_initLayoutMessages('customer/session');
+      $this->_initLayoutMessages('catalog/session');
+      $this->getLayout()->getBlock('head')->setTitle($this->__('Magepay Transactions'));
+      $this->renderLayout();
     }
 }
