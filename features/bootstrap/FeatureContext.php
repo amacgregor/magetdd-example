ff2d0d9ec90cdbacd3bc77352fd564da9fa129ec
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-09-10 07:05:34 -0400

    Update Behat and end of exercise2

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -10,7 +10,8 @@ use Behat\Gherkin\Node\TableNode;
  */
  use MageTest\MagentoExtension\Context\MagentoContext;
  use Behat\Behat\Tester\Exception\PendingException;
- 
+ use \Mage;
+
  class FeatureContext extends MagentoContext implements Context, SnippetAcceptingContext
  {
     /**
@@ -29,7 +30,33 @@ use Behat\Gherkin\Node\TableNode;
      */
     public function iHaveAtLeastOnePreviousTransaction()
     {
-        throw new PendingException();
+      $customer = \Mage::getModel("customer/customer");
+      $customer->setWebsiteId(\Mage::app()->getStore()->getWebsiteId());
+      $customer->loadByEmail('amacgregor@magetdd.com');
+
+      if(!$customer->hasEntityId()){
+        throw new RuntimeException('Test Customer does not exists.');
+      }
+
+      $transactionCollection = \Mage::getModel('magetdd_magepay/transaction')
+        ->getCollection()
+        ->addFieldToFilter('customer_id', $customer->getEntityId());
+
+      if($transactionCollection->getSize() == 0) {
+        $data = array(
+          'transaction_id' => 99999999999,
+          'order_id'       => 888888888888,
+          'state'          => 'processing',
+          'created_at'     => '2015-09-09',
+          'customer_id'    =>  $customer->getEntityId(),
+        );
+
+        $model = \Mage::getModel('magetdd_magepay/transaction');
+        $model->setData($data);
+        $model->save();
+
+        return true;
+      }
     }
 
     /**
